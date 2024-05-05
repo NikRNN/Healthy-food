0daea58d1f22d77bab99a94c2351ee99f32de57d
Nikita <romanovnnikita@gmail.com>
2024-05-05 23:06:02 +0300

    добавлены классы для menu, отредактированы элементы в menu, исправлено смещение страницы при открывании модального окна

--- src/js/script.js
+++ src/js/script.js
@@ -96,46 +96,152 @@ document.addEventListener("DOMContentLoaded", () => {
 
   getTimer(".timer", deadline);
   getClock(deadline);
-});
 
-// вызов и закрытие модального окна
-const modal = document.querySelector(".modal"),
-  btns = document.querySelectorAll("[data-modal"),
-  closeModalBtn = document.querySelector("[data-close]");
+  // вызов и закрытие модального окна
+  const modal = document.querySelector(".modal"),
+    btns = document.querySelectorAll("[data-modal"),
+    closeModalBtn = document.querySelector("[data-close]"),
+    body = document.querySelector("body"),
+    scrollY = calcScroll();
 
-function openModal(list) {
-  list.forEach((elem) => {
-    elem.addEventListener("click", () => {
-      modal.classList.toggle("hide");
-      document.documentElement.style.overflow = "hidden";
-      clearInterval(modalTimerId);
+  function openModal(list) {
+    list.forEach((elem) => {
+      elem.addEventListener("click", () => {
+        modal.classList.toggle("hide");
+        document.documentElement.style.overflow = "hidden";
+        document.body.style.marginRight = `${scrollY}px`;
+        clearInterval(modalTimerId);
+      });
     });
-  });
-}
+  }
 
-function closeModal(btn) {
-  btn.addEventListener("click", () => {
+  function closeModal(btn) {
+    btn.addEventListener("click", () => {
+      modal.classList.toggle("hide");
+      document.documentElement.style.overflow = "scroll";
+      document.body.style.marginRight = `0px`;
+    });
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.classList.toggle("hide");
+        document.documentElement.style.overflow = "scroll";
+        document.body.style.marginRight = `0px`;
+      }
+    });
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Escape") {
+        modal.classList.toggle("hide");
+        document.documentElement.style.overflow = "scroll";
+        document.body.style.marginRight = `0px`;
+      }
+    });
+  }
+
+  openModal(btns);
+  closeModal(closeModalBtn);
+
+  const modalTimerId = setTimeout(() => {
     modal.classList.toggle("hide");
-    document.documentElement.style.overflow = "scroll";
-  });
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.classList.toggle("hide");
-      document.documentElement.style.overflow = "scroll";
-    }
-  });
-  document.addEventListener("keydown", (e) => {
-    if (e.code === "Escape") {
-      modal.classList.toggle("hide");
-      document.documentElement.style.overflow = "scroll";
-    }
-  });
-}
+    document.documentElement.style.overflow = "hidden";
+    document.body.style.marginRight = `${scrollY}px`;
+  }, 3000); //появление модального окна через 5 сек после захода на страницу
 
-openModal(btns);
-closeModal(closeModalBtn);
+  function showModalByScroll() {
+    if (
+      document.documentElement.clientHeight +
+        document.documentElement.scrollTop >=
+      document.documentElement.scrollHeight
+    ) {
+      modal.classList.toggle("hide");
+      document.body.style.marginRight = `${scrollY}px`;
+      document.documentElement.style.overflow = "hidden";
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  } //появление модального окна про прокрутке страницы до низа
 
-const modalTimerId = setTimeout(() => {
-  modal.classList.toggle("hide");
-  document.documentElement.style.overflow = "hidden";
-}, 5000); //появление модального окна через 5 сек после захода на страницу
+  window.addEventListener("scroll", showModalByScroll);
+
+  function calcScroll() {
+    let div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+
+    div.remove();
+
+    return scrollWidth;
+  }
+
+  calcScroll(); //фон за модальным окном не сдвигается
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
+    }
+
+    render() {
+      const div = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        div.classList.add("menu__item");
+      } else {
+        this.classes.forEach((className) => div.classList.add(className));
+      }
+
+      div.innerHTML = `<img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+            </div>`;
+
+      this.parent.append(div);
+    }
+  }
+
+  //создаем элементы в блоке меню
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    450,
+    ".menu__field .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    "Меню “Премиум”",
+    'В меню "Премиум" мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    780,
+    ".menu__field .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    550,
+    ".menu__field .container",
+    "menu__item"
+  ).render();
+});
