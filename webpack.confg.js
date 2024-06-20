d9f82616ef52b6c581cdff603cf551c01c4ce236
Nikita <romanovnnikita@gmail.com>
2024-06-20 20:51:31 +0300

    проблема с webpack

--- webpack.confg.js
+++ webpack.confg.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./js/script.js",
+  entry: "./src/js/script.js",
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist/js",
